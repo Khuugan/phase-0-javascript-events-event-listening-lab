e3bcdb53d27278fbcbd5639a3b50bde8d742868c
Narankhuu Ganbaatar <narankhuuu@gmail.com>
2023-09-24 00:47:02 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
-}
+    const button = document.getElementById('button');
+  
+    button.addEventListener('click', function() {
+        const input = document.getElementById('button');
+
+        function clickAlert() {
+          alert('I was clicked!');
+        }
+        
+        input.addEventListener('click', clickAlert);  
+    });
+  }
